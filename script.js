4e6a7ae5388afa5c09823d238f73098e32135606
victorbrambilla <88890771+victorbrambilla@users.noreply.github.com>
2021-08-21 14:49:13 -0400

    confirmar

---
+++ script.js
@@ -0,0 +1,10 @@
+function menuToggle(){
+    let nav= document.querySelector('nav')
+    if(nav.style.display== 'inline-block'){
+        nav.style.display='none';
+    }else{
+        nav.style.display='inline-block';
+        
+
+    }
+}
